78ba1de1d087b544d6a3a50a2263704b498165e5
ThyW <kamo.bavmesa@gmail.com>
2020-07-14 00:02:02 +0200

    Changes

--- scripts/sync-config.sh
+++ scripts/sync-config.sh
@@ -6,6 +6,7 @@ cp -r ~/.config/i3/status.toml ~/programming/dotfiles/i3/
 cp -r ~/.config/nvim/init.vim ~/programming/dotfiles/nvim/
 cp -r ~/sync-config.sh ~/programming/dotfiles/scripts
 cp -r ~/.bashrc ~/programming/dotfiles/scripts/
+cp -r ~/sync-config.sh ~/programming/dotfiles/scripts/
 
 temp="$PWD"
 
