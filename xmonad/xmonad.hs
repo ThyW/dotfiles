5055c8e908d50e7355d723144e689a25cdde96f0
ThyW <kamo.bavmesa@gmail.com>
2021-01-25 13:03:39 +0100

    Changes

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -78,7 +78,7 @@ binds c =
     , ("M-S-x", spawn "killall xmonad-x86_64-linux")
     , ("M-i", spawn browser)
     , ("M-g", sendMessage $ ToggleGaps)
-    , ("M-S-s" spawn "pavucontrol")
+    , ("M-A-s", spawn "pavucontrol")
     ]
     ++
 
