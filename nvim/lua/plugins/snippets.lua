e77c90ace9449785495500aef9e3a13f76e252aa
ThyW <kamo.bavmesa@gmail.com>
2022-02-11 09:19:23 +0100

    snippet change

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -2,10 +2,12 @@ local ls = require("luasnip")
 local s = ls.parser.parse_snippet
 
 ls.snippets = {
+    all = {
+    	s("shbang", "#!/usr/bin/env $0")
+    },
     rust = {
 	s("ils", "if let Some($1) = $2 {\n    $3\n}"),
     },
     python = {
-    	s("shbang", "#!/usr/bin/env python3")
-    }
+    },
 }
