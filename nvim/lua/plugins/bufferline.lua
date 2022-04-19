193f82ce76864dba73184a3d2d317c970cf9e30c
ThyW <kamo.bavmesa@gmail.com>
2022-04-19 11:54:22 +0200

    nvim: fix bufferline separator colors
    nvim: fix bufferline separator colors

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,12 +1,27 @@
-require'bufferline'.setup{
+require 'bufferline'.setup {
     options = {
-    	numbers = "buffer_id",
+	numbers = "buffer_id",
 	close_command = "bdelete! %d",
 	left_mouse_command = "buffer %d",
 	diagnostics = "nvim_lsp",
 	show_buffer_icons = true,
 	always_show_bufferline = true,
 	separator_style = "slant",
-	offsets = {{filetype = "NvimTree"}}
+	offsets = { { filetype = "NvimTree", text = "File Explorer" } }
     },
+
+    highlights = {
+	separator_selected = {
+	    guifg = "#504945",
+	    guibg = "none"
+	},
+	separator_visible = {
+	    guifg = "#504945",
+	    guibg = "none"
+	},
+	separator = {
+	    guifg = "#504945",
+	    guibg = "none"
+	}
+    }
 }
