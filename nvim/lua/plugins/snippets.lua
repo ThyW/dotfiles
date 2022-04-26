54c072c25422f1243f8f16e484a9af9e4694af22
ThyW <kamo.bavmesa@gmail.com>
2022-04-26 23:07:57 +0200

    nvim: snippet fix

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -2,4 +2,4 @@ local ls = require("luasnip")
 local s = ls.parser.parse_snippet
 
 ls.add_snippets("all", { s("shbang", "#!/usr/bin/env $0") })
-ls.add_snippets("rust", { s("ils", "if let Some($1) = $2 {\n    $3\n}"), s("pdbg", "#[cfg(debug_assertions)]\nprintln!(\"$1\")") })
+ls.add_snippets("rust", { s("ils", "if let Some($1) = $2 {\n    $3\n}"), s("pdbg", "#[cfg(debug_assertions)]\nprintln!(\"$1\");") })
