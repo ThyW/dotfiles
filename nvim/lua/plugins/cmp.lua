adcc8c7bedbb1a26a8af4e7e0376990674ef1b41
ThyW <kamo.bavmesa@gmail.com>
2022-05-04 11:28:41 +0200

    nvim: change cmp autocomplete trigger and format plugins

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,10 +1,10 @@
 local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- load friendly snippets
-require("luasnip.loaders.from_vscode").lazy_load()
+local _ = require("luasnip.loaders.from_vscode").lazy_load()
 
 -- remove tabnine configuration
 --[[ local tabnine = require'cmp_tabnine.config'
@@ -16,73 +16,74 @@ tabnine:setup({
     snippet_placeholder = '..';
 }) ]]
 
-local nvim_cmp = require'cmp'
-local luasnip = require'luasnip'
+local nvim_cmp = require 'cmp'
+local luasnip = require 'luasnip'
 
-nvim_cmp.setup{
-    mapping = {
-	["<c-u>"] = nvim_cmp.mapping.scroll_docs(-4),
-	["<c-d>"] = nvim_cmp.mapping.scroll_docs(4),
-	["<Cr>"] = nvim_cmp.mapping.confirm(),
-	["<c-space>"] = nvim_cmp.mapping.confirm(),
-	["<c-e>"] = nvim_cmp.mapping.close(),
-	["<c-n>"] = nvim_cmp.mapping(function(fallback)
-	    if nvim_cmp.visible() then
-		nvim_cmp.select_next_item()
-	    elseif luasnip.expand_or_jumpable() then
-		luasnip.expand_or_jump()
-	    elseif has_words_before() then
-		nvim_cmp.complete()
-	    else
-		fallback()
-	    end
-	end, { "i", "s" }),
+nvim_cmp.setup {
+	completion = {
+	    autocomplete = false,
+	},
+	mapping = {
+		["<c-u>"] = nvim_cmp.mapping.scroll_docs(-4),
+		["<c-d>"] = nvim_cmp.mapping.scroll_docs(4),
+		["<Cr>"] = nvim_cmp.mapping.confirm(),
+		["<c-space>"] = nvim_cmp.mapping.confirm(),
+		["<c-e>"] = nvim_cmp.mapping.close(),
+		["<c-n>"] = nvim_cmp.mapping(function(fallback)
+			if nvim_cmp.visible() then
+				nvim_cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				nvim_cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 
-	["<c-p>"] = nvim_cmp.mapping(function(fallback)
-	    if nvim_cmp.visible() then
-		nvim_cmp.select_prev_item()
-	    elseif luasnip.jumpable(-1) then
-		luasnip.jump(-1)
-	    else
-		fallback()
-	    end
-	end, { "i", "s" }),
-    },
+		["<c-p>"] = nvim_cmp.mapping(function(fallback)
+			if nvim_cmp.visible() then
+				nvim_cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	},
 
-    sources = {
-	{name = "neorg"},
-	{name = "luasnip"},
-	{name = "nvim_lua"},
-	{name = "nvim_lsp"},
-	{name = "gh_issues"},
-	{name = "path"},
-	{name = "buffer"}
-	-- {name = "cmp_tabnine"},
-    },
+	sources = {
+		{ name = "neorg" },
+		{ name = "luasnip" },
+		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
+		{ name = "gh_issues" },
+		{ name = "path" },
+		{ name = "buffer" }
+		-- {name = "cmp_tabnine"},
+	},
 
-    snippet = {
-	expand = function(args)
-	    luasnip.lsp_expand(args.body)
-	end,
-    },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 
-    formatting = {
-	format = require'lspkind'.cmp_format{
-	    with_text = true,
-	    menu = {
-	    	cmp_tabnine = "[TAB]",
-		buffer = "[BUF]",
-		nvim_lsp = "[LSP]",
-		nvim_lua = "[LUA]",
-		path = "[PATH]",
-		gh_issues = "[Issue]",
-	    }
-	}
-    },
+	formatting = {
+		format = require 'lspkind'.cmp_format {
+			with_text = true,
+			menu = {
+				cmp_tabnine = "[TAB]",
+				buffer = "[BUF]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[LUA]",
+				path = "[PATH]",
+				gh_issues = "[Issue]",
+			}
+		}
+	},
 
-    experimental = {
-	native_menu = false,
-	ghost_text = true
-    },
-
+	experimental = {
+		ghost_text = true
+	},
 }
