f49ad268fbfd5fcd7b2837f982e2e4694c481235
xkapko <536648@mail.muni.cz>
2025-06-05 16:31:35 +0200

    move over to vim.lsp.config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -11,7 +11,6 @@ local M = {
 
 M.config = function()
 	local lsp_settings = require("..plugins.extra.lsp")
-	local lspconfig = lsp_settings.lspconfig
 
 	require("mason").setup({})
 	require("mason-lspconfig").setup({
@@ -25,168 +24,155 @@ M.config = function()
 			"harper_ls",
 			"hls",
 		},
+		automatic_enable = true,
 	})
 
-	require("mason-lspconfig").setup_handlers({
-		function(server)
-			lspconfig[server].setup({
-				on_attach = lsp_settings.on_attach,
-				capabilities = lsp_settings.capabilities,
-				flags = {
-					debounce_text_changes = 150,
+	vim.lsp.config("*", {
+		on_attach = lsp_settings.on_attach,
+		capabilities = lsp_settings.capabilities,
+		flags = {
+			debounce_text_changes = 150,
+		},
+	})
+	vim.lsp.config("harper_ls", {
+		filetypes = {
+			"c",
+			"cpp",
+			"cs",
+			"gitcommit",
+			"go",
+			"html",
+			"java",
+			"javascript",
+			"lua",
+			"markdown",
+			"nix",
+			"python",
+			"ruby",
+			"rust",
+			"swift",
+			"toml",
+			"typescript",
+			"typescriptreact",
+			"haskell",
+			"cmake",
+			-- "typst",
+			"php",
+			"dart",
+		},
+		settings = {
+			["harper-ls"] = {
+				userDictPath = "",
+				fileDictPath = "",
+				linters = {
+					SpellCheck = true,
+					SpelledNumbers = false,
+					AnA = true,
+					SentenceCapitalization = true,
+					UnclosedQuotes = true,
+					WrongQuotes = false,
+					LongSentences = true,
+					RepeatedWords = true,
+					Spaces = true,
+					Matcher = true,
+					CorrectNumberSuffix = true,
 				},
-			})
-		end,
-		["harper_ls"] = function()
-			lspconfig["harper_ls"].setup({
-				filetypes = {
-					"c",
-					"cpp",
-					"cs",
-					"gitcommit",
-					"go",
-					"html",
-					"java",
-					"javascript",
-					"lua",
-					"markdown",
-					"nix",
-					"python",
-					"ruby",
-					"rust",
-					"swift",
-					"toml",
-					"typescript",
-					"typescriptreact",
-					"haskell",
-					"cmake",
-					"typst",
-					"php",
-					"dart",
+				codeActions = {
+					ForceStable = false,
 				},
-				settings = {
-					["harper-ls"] = {
-						userDictPath = "",
-						fileDictPath = "",
-						linters = {
-							SpellCheck = true,
-							SpelledNumbers = false,
-							AnA = true,
-							SentenceCapitalization = true,
-							UnclosedQuotes = true,
-							WrongQuotes = false,
-							LongSentences = true,
-							RepeatedWords = true,
-							Spaces = true,
-							Matcher = true,
-							CorrectNumberSuffix = true,
-						},
-						codeActions = {
-							ForceStable = false,
-						},
-						markdown = {
-							IgnoreLinkTitle = false,
-						},
-						diagnosticSeverity = "hint",
-						isolateEnglish = false,
-						dialect = "American",
+				markdown = {
+					IgnoreLinkTitle = false,
+				},
+				diagnosticSeverity = "hint",
+				isolateEnglish = false,
+				dialect = "American",
+			},
+		},
+	})
+	vim.lsp.config("lua_ls", {
+		on_attach = lsp_settings.on_attach,
+		flags = {
+			debounce_text_changes = 150,
+		},
+		settings = {
+			Lua = {
+				-- this is still not supported
+				format = {
+					enable = true,
+					defaultConfig = {
+						indent_style = "space",
+						indent_size = "2",
 					},
 				},
-			})
-		end,
-		["lua_ls"] = function()
-			lspconfig["lua_ls"].setup({
-				on_attach = lsp_settings.on_attach,
-				flags = {
-					debounce_text_changes = 150,
+				runtime = {
+					version = "LuaJIT",
+					path = lsp_settings.rtp,
 				},
-				settings = {
-					Lua = {
-						-- this is still not supported
-						format = {
-							enable = true,
-							defaultConfig = {
-								indent_style = "space",
-								indent_size = "2",
-							},
-						},
-						runtime = {
-							version = "LuaJIT",
-							path = lsp_settings.rtp,
-						},
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true),
-						},
-						telemetry = {
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				telemetry = {
+					enable = false,
+				},
+				hint = { enable = true },
+			},
+		},
+	})
+	vim.lsp.config("rust_analyzer", {
+		on_attach = lsp_settings.on_attach,
+		cmd = { "rustup", "run", "nightly", "rust-analyzer" },
+		capabilities = lsp_settings.capabilities,
+		server = {
+			settings = {
+				-- yes
+				Rust = {
+					procMacro = {
+						enable = true,
+					},
+					checkOnSave = {
+						enable = true,
+						command = "clippy",
+					},
+					completion = {
+						postfix = {
 							enable = false,
 						},
-						hint = { enable = true },
+						snippets = {
+							enable = false,
+							custom = {},
+						},
+					},
+					imports = {
+						prefix = "crate",
 					},
 				},
-			})
-		end,
-		["rust_analyzer"] = function()
-			lspconfig["rust_analyzer"].setup({
-				on_attach = function(c, b)
-					lsp_settings.on_attach(c, b)
-				end,
-				cmd = { "rustup", "run", "nightly", "rust-analyzer" },
-				capabilities = lsp_settings.capabilities,
-				server = {
-					settings = {
-						-- yes
-						Rust = {
-							procMacro = {
-								enable = true,
-							},
-							checkOnSave = {
-								enable = true,
-								command = "clippy",
-							},
-							completion = {
-								postfix = {
-									enable = false,
-								},
-								snippets = {
-									enable = false,
-									custom = {},
-								},
-							},
-							imports = {
-								prefix = "crate",
-							},
+			},
+		},
+	})
+	vim.lsp.config("pyright", {
+		on_attach = lsp_settings.on_attach,
+		capabilities = lsp_settings.capabilities,
+		cmd = { "pyright-langserver", "--stdio" },
+		server = {
+			settings = {
+				Python = {
+					formatting = {
+						provider = "black",
+						blackPath = "/home/zir/.local/bin/black",
+					},
+					analysis = {
+						typeCheckingMode = "basic",
+						inlayHints = {
+							variableTypes = true,
+							functionRetrunTypes = true,
 						},
 					},
 				},
-			})
-		end,
-		["pyright"] = function()
-			lspconfig["pyright"].setup({
-				on_attach = lsp_settings.on_attach,
-				capabilities = lsp_settings.capabilities,
-				cmd = { "pyright-langserver", "--stdio" },
-				server = {
-					settings = {
-						Python = {
-							formatting = {
-								provider = "black",
-								blackPath = "/home/zir/.local/bin/black",
-							},
-							analysis = {
-								typeCheckingMode = "basic",
-								inlayHints = {
-									variableTypes = true,
-									functionRetrunTypes = true,
-								},
-							},
-						},
-					},
-				},
-			})
-		end,
+			},
+		},
 	})
 end
 
