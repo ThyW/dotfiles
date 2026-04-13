8cb3d44adc2969d00a4ba9f21d3864d2bf9f347b
xkapko <xkapko@fi.muni.cz>
2026-04-14 00:21:17 +0200

    nvim: better comments

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,7 @@ local M = {
 
 local patch_colors = function()
 	local highlights = {
+		"highlight Comment guifg=#fabd2f",
 		"highlight SignColumn guibg=none",
 		"highlight LineNr ctermfg=240",
 		"highlight CursorLineNr cterm=bold gui=bold guibg=none",
